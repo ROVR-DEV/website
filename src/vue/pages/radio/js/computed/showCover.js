ec76586c51362c18d70efa1deec94bfb4d0fc2be
grande2k <amirkek80@gmail.com>
2023-10-25 20:56:58 +0500

    check for mobile and desktop cover

--- src/vue/pages/radio/js/computed/showCover.js
+++ src/vue/pages/radio/js/computed/showCover.js
@@ -3,9 +3,9 @@
 export default function showCover() {
 	if(this.radio) {
 		if(screen.width >= 770) {
-			return this.radio.show.cover_desktop.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched])
+			return this.radio.show.cover_desktop ? this.radio.show.cover_desktop.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover
 		} else {
-			return this.radio.show.cover_mobile.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched])
+			return this.radio.show.cover_mobile ? this.radio.show.cover_mobile.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover
 		}
 	} else {
 		return 'https://coolbackgrounds.io/images/backgrounds/black/black-contour-f41038db.svg'
