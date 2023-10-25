21714f8d469ee6d28271c31c6a5a3a4b4f159506
grande2k <amirkek80@gmail.com>
2023-10-25 21:00:49 +0500

    check for mobile and desktop cover (bug fix)

--- src/vue/pages/radio/js/computed/showCover.js
+++ src/vue/pages/radio/js/computed/showCover.js
@@ -3,9 +3,9 @@
 export default function showCover() {
 	if(this.radio) {
 		if(screen.width >= 770) {
-			return this.radio.show.cover_desktop ? this.radio.show.cover_desktop.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover
+			return this.radio.show.cover_desktop ? this.radio.show.cover_desktop.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched])
 		} else {
-			return this.radio.show.cover_mobile ? this.radio.show.cover_mobile.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover
+			return this.radio.show.cover_mobile ? this.radio.show.cover_mobile.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched]) : this.radio.show.cover.replace(/localhost|lp/gi, (matched) => this.replaceCover[matched])
 		}
 	} else {
 		return 'https://coolbackgrounds.io/images/backgrounds/black/black-contour-f41038db.svg'
