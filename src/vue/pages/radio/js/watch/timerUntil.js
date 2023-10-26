014fc3c233de08a2214032ad7e793901b19e11a4
grande2k <amirkek80@gmail.com>
2023-10-26 18:16:13 +0500

    show change fixes (transission and fade out) upgrade

--- src/vue/pages/radio/js/watch/timerUntil.js
+++ src/vue/pages/radio/js/watch/timerUntil.js
@@ -15,38 +15,22 @@ export default function timerUntil(oldVal, newVal) {
 		        } else {
 		          clearInterval(fadeOutInterval);
 		        }
-		     }, 250);
+		     }, 200);
 
 			setTimeout(() => {
 				player.pause();
 				player.setAttribute('src', jingle);
-			}, 3000);
+			}, 2000);
 
 			setTimeout(() => {
 				player.play();
 				player.volume = 1;
 
-				this.$refs.radio.classList.remove('show-cover');
-				this.$refs.radio.classList.add('hide-cover');
-
-				setTimeout(() => {
-					this.$refs.radio.classList.remove('visible');
-				}, 400);
-
-				setTimeout(() => {
-					this.$refs.radio.classList.remove('hide-cover');
-					this.$refs.radio.classList.add('show-cover');
-				}, 2000);
-
-				setTimeout(() => {
-					this.$refs.radio.classList.add('visible');
-				}, 2400);
-
 				player.addEventListener('ended', () => {
 					player.setAttribute('src', this.radio.stream_url);
 					player.play();
 				});
-			}, 4000);
+			}, 3000);
 		}
 	}
 }
